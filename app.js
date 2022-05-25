f1d48fcd3fe6c5f8da4f9b3574861c6d214f6db5
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-05-25 16:05:35 -0700

    added menu items in both languages

--- app.js
+++ app.js
@@ -2,24 +2,3 @@ let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
-
-
-const mapDiv = document.querySelector('#map-div');
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-    mapDiv.appendChild(map);
-  }
-  
- initMap();
