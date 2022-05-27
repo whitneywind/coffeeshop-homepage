b5bf2e4061a21d9b449f438a72fb41ae91cdf72f
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-05-27 13:05:55 -0700

    added more language functionality

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+
+// toggle button text on mouseover
+const langBtn = document.querySelector('#lang-btn');
+langBtn.addEventListener('mouseover', textToggle);
+langBtn.addEventListener('mouseout', textToggle)
+
+function textToggle() {
+    langBtn.innerText === 'lang (eng)' ? langBtn.innerText = '한국어로' : langBtn.innerText = 'lang (eng)';
+}
